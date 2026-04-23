f7e9e292c6e026300f3777b27923c23888b89aae
Fabian Radlow <fabianradlow@gmail.com>
2026-04-23 09:51:23 +0200

    Finished this exercise and now Im on my way to get Coffee! :33

--- src/idh/java/ATM.java
+++ src/idh/java/ATM.java
@@ -4,7 +4,8 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 public class ATM {
-	int accountBalance = 100;
+	int totalReserve = 10000; 
+	int[] accounts = new int[] {10000, 5000, 1000}; 
 
 	/**
 	 * Main command loop of the ATM Asks the user to enter a number, and passes this
@@ -16,21 +17,29 @@ public class ATM {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		while (true) {
 			try {
+				System.out.print("Enter your account id");
+				int accountId = Integer.parseInt(br.readLine());
+				int accountBalance = accounts[accountId]; 
+				System.out.print("You currently have " + accounts[accountId] + " In your Bank account");
 				System.out.print("Enter the amount to withdraw: ");
 				int amount = Integer.parseInt(br.readLine());
-				cashout(amount);
+				
+				cashout(amount, accountId);
+
 			} catch (Exception e) {
 				break;
 			}
 		}
 	}
-
-	public void cashout(int amount) {
-		if (amount < accountBalance) {
-			accountBalance = accountBalance - amount;
+	public void cashout(int amount, int accountId) {
+		if(totalReserve < amount)
+		{ System.out.println("not enough money in this device");}
+		if (amount <= accounts[accountId]) {
+			accounts[accountId] = accounts[accountId] - amount;
 			System.out.println("Ok, here is your money, enjoy!");
+			System.out.println("You have " + accounts[accountId] + "$ left in your account");
 		} else {
-			System.out.println("Sorry, not enough money in the bank.");
+			System.out.println("Sorry, not enough money in the your bank.");
 		}
 
 	};
