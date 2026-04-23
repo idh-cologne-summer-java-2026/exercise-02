fdf3c29101bd1d044b9936e288b6bf1d106ef246
joric <joric@192.168.178.129>
2026-04-23 17:09:21 +0200

    aufgabe 2 mit check ob es den Acc gibt

--- src/idh/java/ATM.java
+++ src/idh/java/ATM.java
@@ -5,7 +5,10 @@ import java.io.InputStreamReader;
 
 public class ATM {
 	int accountBalance = 100;
-
+	int[] accounts = {1000, 250, 3000, 50};
+	int accountID;
+	int cash = 650;
+	
 	/**
 	 * Main command loop of the ATM Asks the user to enter a number, and passes this
 	 * number to the function cashout(...) which actually does the calculation and
@@ -16,26 +19,56 @@ public class ATM {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		while (true) {
 			try {
+				
+				
+				while(true) {
+					
+					try {
+						System.out.print("Enter your account ID: ");
+						accountID = Integer.parseInt(br.readLine());
+						if(accountID >= accounts.length) {
+							System.out.println("Account does not exist, please try again.");
+							
+						}
+						if(accountID < accounts.length) {
+							break;
+						}
+						
+					}catch (Exception e) {
+						break;
+					}
+					
+				}  		
+				
+				System.out.println("Your Balance is: " + accounts[accountID]);
+				
 				System.out.print("Enter the amount to withdraw: ");
 				int amount = Integer.parseInt(br.readLine());
-				cashout(amount);
+				cashout(amount, accountID);
+				
 			} catch (Exception e) {
 				break;
 			}
 		}
 	}
 
-	public void cashout(int amount) {
-		if (amount < accountBalance) {
-			accountBalance = accountBalance - amount;
+	public void cashout(int amount, int accountID ) {
+		
+		if(amount <= cash) {
+			
+			if (amount <= accounts[this.accountID]) {	
+			accounts[this.accountID] = accounts[this.accountID] - amount;
+			cash = cash - amount;
 			System.out.println("Ok, here is your money, enjoy!");
-		} else {
+			} else {
 			System.out.println("Sorry, not enough money in the bank.");
+			}
+		} else {
+			System.out.println("not enough cash in the ATM :(");
 		}
+	}
 
-	};
-
-	/**
+	/*
 	 * Launches the ATM
 	 */
 	public static void main(String[] args) {
