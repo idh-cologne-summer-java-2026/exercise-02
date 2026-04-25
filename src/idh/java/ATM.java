1d5de0d80dfb884440ce3502a4d99da1ad890ee4
kayakalkuhl <buckenkerim5@gmail.com>
2026-04-25 16:00:28 +0200

    fertige aufgabe ig

--- src/idh/java/ATM.java
+++ src/idh/java/ATM.java
@@ -1,11 +1,14 @@
 package idh.java;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class ATM {
-	int accountBalance = 100;
 
+	int ATMBalance = 1000;
+	int[] Kontonummern = {1234, 2345, 3456, 4567, 5678};
+	int[] Kontostände ={777, 333, 969, 67, 420};
 	/**
 	 * Main command loop of the ATM Asks the user to enter a number, and passes this
 	 * number to the function cashout(...) which actually does the calculation and
@@ -16,21 +19,54 @@ public class ATM {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		while (true) {
 			try {
-				System.out.print("Enter the amount to withdraw: ");
+
+				System.out.println("Sgeden , Was Nummer?");
+				int KontonummerInput = Integer.parseInt(br.readLine());
+
+				int lastIndex = -1; 
+				for (int i = 0; i < Kontonummern.length; i++) {
+					if (Kontonummern[i] == KontonummerInput) {
+						lastIndex = i; 
+						break; 
+					}
+				}
+					if (lastIndex == -1) {
+					System.out.println("oh schade die gibts nicht nummer. Please try again.");
+					continue; 
+				} 
+				System.out.println("Dein geld !:" + Kontostände[lastIndex]);
+
+				System.out.println("Verfügbar:" + ATMBalance);
+		
+				System.out.println("was willst du abheben: ");
 				int amount = Integer.parseInt(br.readLine());
-				cashout(amount);
-			} catch (Exception e) {
+					cashout(lastIndex,amount);
+			} catch (IOException | NumberFormatException e) {
 				break;
 			}
 		}
 	}
 
-	public void cashout(int amount) {
-		if (amount < accountBalance) {
-			accountBalance = accountBalance - amount;
+	public void cashout(int lastIndex, int amount) {
+		if (amount  > ATMBalance) {
+			System.out.println("Der Automat IST ARM ; später versuchen");
+			return;
+		}
+		
+		
+		if (amount <= Kontostände[lastIndex] && amount <= ATMBalance) { 
+			
+			Kontostände[lastIndex] -= amount;
+			ATMBalance -= amount;  
+			
 			System.out.println("Ok, here is your money, enjoy!");
-		} else {
-			System.out.println("Sorry, not enough money in the bank.");
+			
+		
+		} else if (amount > Kontostände[lastIndex]){
+			System.out.println("Nicht genug Geld auf dem Konto, denk mal drüber nach");
+			
+		} else if (amount > ATMBalance) {
+			System.out.println("Geldautomat leergefegt");
 		}
 
 	};
