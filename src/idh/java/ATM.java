2322a36fd0e57b9ab9820238342c7f027c447b0e
test <test@DESKTOP-C0QB574>
2026-04-28 21:42:25 +0200

    Übung 2 fertig

--- src/idh/java/ATM.java
+++ src/idh/java/ATM.java
@@ -5,7 +5,8 @@ import java.io.InputStreamReader;
 
 public class ATM {
 	int accountBalance = 100;
-
+    int atmCredit =1;
+    Konto [] accounts  = new Konto [3];
 	/**
 	 * Main command loop of the ATM Asks the user to enter a number, and passes this
 	 * number to the function cashout(...) which actually does the calculation and
@@ -13,26 +14,55 @@ public class ATM {
 	 * loop breaks and the program exists
 	 */
 	public void run() {
+		accounts (); //acounts are made here
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		while (true) {
 			try {
+				System.out.println ("Welcome. Please, enter your account number");
+				int number = Integer.parseInt(br.readLine());
+				if (checkNumber (number)) {
 				System.out.print("Enter the amount to withdraw: ");
 				int amount = Integer.parseInt(br.readLine());
-				cashout(amount);
+				cashout (amount, number);
+				}else {
+					System.out.println ("Sorry, this account doesn't exist.");
+				}
+	
 			} catch (Exception e) {
 				break;
 			}
 		}
 	}
+	private void accounts() {
+		// TODO Auto-generated method stub
+		
+	}
+	public boolean checkNumber (int number) // see if an account with this number exists
+	{
+		for (int i =0; i< this.accounts.length; i++) {
+			if (this.accounts[i].getNumber () == number ) {
+			 return true;
+			} 
+			 }
+				return false;	
+	}
+	public void cashout(int amount, int number) {
+		int accountCredit = 0; // credit of the account that corresponds to number
+		Konto account = null; // just a variable to save the account found
 
-	public void cashout(int amount) {
-		if (amount < accountBalance) {
+		for (int i =0; i < accounts.length; i++) {
+			accounts[i] = new Konto (i, amount);}
+			// search for the rigth accounts credit
+		if (amount <= accountBalance) {
 			accountBalance = accountBalance - amount;
 			System.out.println("Ok, here is your money, enjoy!");
-		} else {
-			System.out.println("Sorry, not enough money in the bank.");
+		} else if (amount> atmCredit){
+			System.out.println("Sorry, the ATM doesn't have enough money");
 		}
-
+		
+		else {
+			System.out.println("Sorry, you don't have enough money in the bank.");
+		}
 	};
 
 	/**
