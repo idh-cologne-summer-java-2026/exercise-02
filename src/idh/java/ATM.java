069f82c20a33850de68191b5a843f1c1fd108195
Hannah Frerich <hfauss@proton.me>
2026-04-28 11:32:01 +0200

    Hausaufgabe erledigt.

--- src/idh/java/ATM.java
+++ src/idh/java/ATM.java
@@ -1,11 +1,10 @@
 package idh.java;
-
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.io.*;
 
 public class ATM {
-	int kontostand = 500;
+	int[] kontostaende = new int[100]; // Accounts von 1-99
+	int geldImAutomat = 350;
+	int accountNumber;
 	/**
 	 * Main command loop of the ATM
 	 * Asks the user to enter a number, and passes this number to the function cashout(...) 
@@ -17,6 +16,12 @@ public class ATM {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		while(true) {
 			try {
+				System.out.println("Enter your account number: ");
+				accountNumber = Integer.parseInt(br.readLine());
+				if (accountNumber < 1 || accountNumber >= 100) {
+					System.out.println("Invalid account number. Please enter a number between 1 and 99.");
+					continue;
+				}
 				System.out.print("Enter the amount to withdraw: ");
 				int amount = Integer.parseInt(br.readLine());
 				cashout(amount);
@@ -32,13 +37,22 @@ public class ATM {
 	}
 	
 	public void cashout(int amount) {
-		if (kontostand >= amount && amount > 0){
-			kontostand -= amount;
-			System.out.println("Here is your money: " + amount +"EUR. Enjoy! Your current balance is: " + kontostand +"EUR.");
+		//Sicherstellung, dass die Beträge des Abhebens und des Geldes im Automaten zu einer (für den User) positiven Ausgabe führen
+		if (amount <= 0) {
+			System.out.println("Please enter a positive amount.");
+			return;
 		}
-		else {
-			System.out.println("Sorry, that is not possible. Please enter a valid amount to withdraw. Your current balance is: " + kontostand +"EUR.");
+		if (amount > geldImAutomat) {
+			System.out.println("Sorry, the ATM only has " + geldImAutomat + " EUR available. Please enter a valid amount to withdraw.");
+			return;
 		}
+		if (kontostaende[accountNumber] < amount) {
+			System.out.println("Sorry, that is not possible. Your current balance is: " + kontostaende[accountNumber] + " EUR. Please enter a valid amount to withdraw.");
+			return;
+		}
+		kontostaende[accountNumber] -= amount;
+		geldImAutomat -= amount;
+		System.out.println("Here is your money: " + amount +"EUR. Enjoy! Your current balance is: " + kontostaende[accountNumber] +"EUR.");
 	};
 	
 	/**
@@ -46,6 +60,10 @@ public class ATM {
 	 */
 	public static void main(String[] args) {
 		ATM atm = new ATM();
+		// hier wird für jedes Konto 500EUR festgelegt
+		for(int i = 1; i < 100; i++) {
+			atm.kontostaende[i] = 500;
+		}
 		atm.run();
 	};
 };
