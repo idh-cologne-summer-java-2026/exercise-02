0d7f8daccce0706896084598e80e77aa62abefba
ulbri <ulbri@LAPTOP-7LSEGFBI>
2026-04-28 23:27:49 +0200

    Done with Exercise-02.

--- src/idh/java/ATM.java
+++ src/idh/java/ATM.java
@@ -2,45 +2,81 @@ package idh.java;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class ATM {
-	int accountBalance = 100;
-
-	/**
-	 * Main command loop of the ATM Asks the user to enter a number, and passes this
-	 * number to the function cashout(...) which actually does the calculation and
-	 * produces money. If the user enters anything else than an integer number, the
-	 * loop breaks and the program exists
-	 */
-	public void run() {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	Customer selectedCustomer;
+	int disposableCash = 1700;
+	
+	
+	public void run(Customer[] customersArray) {
 		while (true) {
-			try {
-				System.out.print("Enter the amount to withdraw: ");
-				int amount = Integer.parseInt(br.readLine());
-				cashout(amount);
-			} catch (Exception e) {
-				break;
+			int accountNumber = queryAccount();	
+			selectedCustomer = null;
+			for (int i = 0; i < customersArray.length; i++) { 
+				if (customersArray[i].getAccountNumber() == accountNumber) {
+					selectedCustomer = customersArray[i];
+					break;
+				}
+			}
+			if (selectedCustomer != null) {
+					int amount = queryAmount();
+					cashout(selectedCustomer, amount);
+				} else {
+					System.out.println("The account number you have entered doesn't exist. Please try again.");
 			}
 		}
 	}
-
-	public void cashout(int amount) {
-		if (amount < accountBalance) {
-			accountBalance = accountBalance - amount;
-			System.out.println("Ok, here is your money, enjoy!");
-		} else {
-			System.out.println("Sorry, not enough money in the bank.");
+	
+	public int queryAccount() {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		while (true) {
+			try {
+				System.out.print("Enter your account number: ");
+				int accountNumber = Integer.parseInt(br.readLine());
+				return accountNumber;
+			} catch (Exception e) {
+				System.out.println("Invalid entry. Please try again!");
+				}
+			}
+	}
+	
+	public int queryAmount() {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		while (true) {
+				try {
+					System.out.print("Enter the amount to withdraw: ");
+					int amount = Integer.parseInt(br.readLine());
+					return amount;
+				} catch (Exception e) {
+					System.out.println("Invalid entry. Please try again!");
+				}
+		}
 		}
 
-	};
+	 
+	public void cashout(Customer selectedCustomer, int amount) {
+		if (amount > disposableCash) {
+				System.out.println("Sorry, the ATM doesn't have that much cash anymore.");
+		} else { 			
+				int selectedCustomerBalance[] = selectedCustomer.getCustomerBasics();
+				if (amount <= selectedCustomerBalance[1]) {
+					this.disposableCash = disposableCash - amount;
+					int accountBalance = selectedCustomerBalance[1] - amount;
+					selectedCustomer.setAccountBalance(accountBalance);
+					System.out.println("Ok, here is your money, enjoy!");
+				} else {
+					System.out.println("Sorry, not enough money in your bank account.");
+				}
+		}
+	}
 
-	/**
-	 * Launches the ATM
-	 */
 	public static void main(String[] args) {
+		Customer[] customersArray = new Customer[2];
+		customersArray[0] = new Customer(123, 500);
+		customersArray[1] = new Customer(456, 1500);
 		ATM atm = new ATM();
-		atm.run();
+		atm.run(customersArray);
 	};
 
 }
