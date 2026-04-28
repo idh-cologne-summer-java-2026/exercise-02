d72bee6c5cf502ec08b545109c1f68cd5b89e039
yonca <yonca@Yonca.fritz.box>
2026-04-28 21:56:24 +0200

    Versuch Übung 2

--- src/idh/java/ATM.java
+++ src/idh/java/ATM.java
@@ -5,18 +5,27 @@ import java.io.InputStreamReader;
 
 public class ATM {
 	int accountBalance = 100;
-
+    int Geldautomat = 6000;
+    int accountNummber;
 	/**
 	 * Main command loop of the ATM Asks the user to enter a number, and passes this
 	 * number to the function cashout(...) which actually does the calculation and
 	 * produces money. If the user enters anything else than an integer number, the
 	 * loop breaks and the program exists
 	 */
+	
+	
 	public void run() {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		while (true) {
+			
+			System.out.print("Enter your account number: ");
 			try {
-				System.out.print("Enter the amount to withdraw: ");
+				System.out.print("Enter your account number: ");
+				accountNummber = Integer.parseInt(br.readLine());
+				// if ?
+			}
+			{System.out.print("Enter the amount to withdraw: ");
 				int amount = Integer.parseInt(br.readLine());
 				cashout(amount);
 			} catch (Exception e) {
@@ -24,13 +33,15 @@ public class ATM {
 			}
 		}
 	}
-
+    
 	public void cashout(int amount) {
 		if (amount < accountBalance) {
 			accountBalance = accountBalance - amount;
 			System.out.println("Ok, here is your money, enjoy!");
 		} else {
 			System.out.println("Sorry, not enough money in the bank.");
+		} if (amount >Geldautomat) {
+			System.out.println("Sorry, not enough money in the ATM.");
 		}
 
 	};
