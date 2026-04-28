8b5fb22aab2768daf2044d8e9f8e74d3ebafee70
Theodoro Duchenne <Theodoroduchenne@gmail.com>
2026-04-28 12:05:51 +0200

    Bitte funktioniere 2.0

--- src/idh/java/ATM.java
+++ src/idh/java/ATM.java
@@ -4,7 +4,14 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 public class ATM {
-	int accountBalance = 100;
+
+	int ATMBalance = 1000;
+
+	int idex;
+	
+	int[] accountBalance = {500, 500, 500};
+
+	int[] accounts = {123, 444, 561};
 
 	/**
 	 * Main command loop of the ATM Asks the user to enter a number, and passes this
@@ -16,9 +23,16 @@ public class ATM {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		while (true) {
 			try {
-				System.out.print("Enter the amount to withdraw: ");
-				int amount = Integer.parseInt(br.readLine());
-				cashout(amount);
+				System.out.println("Enter your Account ID");
+				int ID = Integer.parseInt(br.readLine());
+				if (checkaccount(ID)) {
+					System.out.println("Account Balance: " + accountBalance[idex]);
+					System.out.print("Enter the amount to withdraw: ");
+					int amount = Integer.parseInt(br.readLine());
+					cashout(amount);
+				} else {
+					System.out.println("Unknown ID");
+				}
 			} catch (Exception e) {
 				break;
 			}
@@ -26,14 +40,30 @@ public class ATM {
 	}
 
 	public void cashout(int amount) {
-		if (amount < accountBalance) {
-			accountBalance = accountBalance - amount;
+		if (amount <= accountBalance[idex] && amount <= ATMBalance && amount != 0) {
+			accountBalance[idex] = accountBalance[idex] - amount;
+			ATMBalance = ATMBalance - amount;
 			System.out.println("Ok, here is your money, enjoy!");
-		} else {
-			System.out.println("Sorry, not enough money in the bank.");
+		} else if (amount > accountBalance[idex]) {
+			System.out.println("BROKE.");
+		} else if (amount > ATMBalance) {
+			System.out.println("Sorry, not enough money in this bankmachine.");
+		} else if (amount == 0) {
+			System.out.println("Operation cancelled");
 		}
 
 	};
+
+	public boolean checkaccount(int id) {
+		boolean r = false;
+		for (idex = 0; idex < accounts.length; idex++) {
+			if (id == accounts[idex]) {
+				r = true;
+				break;
+			}
+		}
+		return r;
+	}
 
 	/**
 	 * Launches the ATM
